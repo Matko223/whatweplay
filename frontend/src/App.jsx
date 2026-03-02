911820281f2b4386b2d1eb114f901571f55f10bc
Martin Valapka <martin.valapka1@gmail.com>
2026-03-02 19:34:30 +0100

    Add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-900">
+      <Navbar />
       <Home />
     </div>
   )
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="bg-gray-800 text-white px-12 py-3 mx-auto flex items-center">
+        <span className="text-3xl font-black tracking-tighter leading-none uppercase flex-1">
+              WhatWe<span className="text-blue-500">Play</span>
+        </span>
+    </nav>
+  );
+}
+
+export default Navbar;``
