95bba8bf7bd315c879505883afab4d09a5f1c8cf
Martin Valapka <martin.valapka1@gmail.com>
2026-03-02 22:37:38 +0100

    Add redirect to homepage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="bg-gray-800 text-white px-12 py-3 mx-auto flex items-center">
+        <Link to="/">
         <span className="text-3xl font-black tracking-tighter leading-none uppercase flex-1 italic">
               WhatWe<span className="text-blue-500 italic">Play</span>
         </span>
+        </Link>
     </nav>
   );
 }
