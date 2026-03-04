550c7920e45cf2d83e748c30e9af3ecbdb98b949
Martin Valapka <martin.valapka1@gmail.com>
2026-03-04 21:05:14 +0100

    Changed navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="bg-gray-800 text-white px-12 py-3 mx-auto flex items-center">
+    <nav className="bg-slate-900 text-white px-6 py-3 mx-auto flex items-center border-b border-slate-700 w-full">
         <Link to="/">
         <span className="text-3xl font-black tracking-tighter leading-none uppercase flex-1 italic">
               WhatWe<span className="text-blue-500 italic">Play</span>
